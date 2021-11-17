839db3b36c3787b043de4c7b983112674c147ffc
Dawid Nowak <nowak_dawid@hotmail.com>
2021-11-17 09:58:18 +0100

    ValueObject, Currency, unit tests

---
+++ src/Domain/ValueObjects/Currency.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.ValueObjects
+{
+    public class Currency : ValueObject
+    {
+        private readonly CurrencyRepository _repo = new();
+
+        public readonly string IsoCode;
+        public readonly string Symbol;
+
+        private Currency(string isoCode, string symbol)
+        {
+            IsoCode = isoCode;
+            Symbol = symbol;
+        }
+
+        public Currency(string isoCode)
+        {
+            var curr = _repo.Get(isoCode);
+            if (curr.Equals(Empty))
+            {
+                throw new ArgumentException("Invalid ISO Currency Code");
+            }
+
+            IsoCode = curr.IsoCode;
+            Symbol = curr.Symbol;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return IsoCode;
+            yield return Symbol;
+        }
+
+        public static Currency Empty => new(string.Empty, string.Empty);
+
+        internal class CurrencyRepository
+        {
+            private static readonly Dictionary<string, Currency> Currencies = new()
+            {
+                { "PLN", new("PLN", "zł") },
+                { "USD", new("USD", "$") },
+                { "EUR", new("EUR", "€") }
+            };
+
+            public Currency Get(string isoCode)
+            {
+                return string.IsNullOrWhiteSpace(isoCode) == false
+                    && Currencies.TryGetValue(isoCode, out var curr) ? curr : Empty;
+            }
+        }
+    }
+
+
+}
---
+++ tests/Domain.UnitTests/ValueObjects/CurrencyTests.cs
@@ -0,0 +1,36 @@
+﻿using Domain.ValueObjects;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Domain.UnitTests.ValueObjects
+{
+    public class CurrencyTests
+    {
+        private static Currency.CurrencyRepository CurrencyRepo = new();
+
+        [Fact]
+        public void ctor_SHOULD_instantiate_Currency_WHEN_valid_isoCode_is_passed()
+        {
+            //Arrange
+            var isoCode = "PLN";
+            var expectedCurr = CurrencyRepo.Get(isoCode);
+
+            //Act
+            var actualCurr = new Currency(isoCode);
+
+            //Assert
+            actualCurr.Should().Be(expectedCurr);
+        }
+
+        [Fact]
+        public void ctor_SHOULD_throw_ArgumentException_WHEN_invalid_isoCode_is_passed()
+        {
+            //Arrange
+            Action action = () => new Currency("Fake iso code");
+
+            //Act & Assert
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
